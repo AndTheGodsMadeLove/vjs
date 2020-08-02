4faf0eadc0683a8fdc3b5aabcd782e3c5dc1e3a0
AndTheGodsMadeLove <info@samuelweber.at>
2020-08-02 15:05:29 +0200

    updated readme and store

--- readme.md
+++ readme.md
@@ -1,6 +1,19 @@
 [![GitHub license](https://img.shields.io/github/license/AndTheGodsMadeLove/vjs.svg)](https://github.com/AndTheGodsMadeLove/vjs/blob/master/LICENSE) [![GitHub tag](https://img.shields.io/github/tag/AndTheGodsMadeLove/vjs.svg)](https://GitHub.com/AndTheGodsMadeLove/vjs/tags/) ![GitHub commits](https://img.shields.io/github/commits-since/AndTheGodsMadeLove/vjs/v0.1-alpha.svg) [![GitHub contributors](https://img.shields.io/github/contributors/AndTheGodsMadeLove/vjs.svg)](https://GitHub.com/AndTheGodsMadeLove/vjs/graphs/contributors/) [![Github all releases](https://img.shields.io/github/downloads/AndTheGodsMadeLove/vjs/total.svg)](https://GitHub.com/AndTheGodsMadeLove/vjs/releases/)
 
 ## VJS
-This project is still looking for a great name... please help! :pray:
 
-### ToDo: README ;)
+The Goal of VJS is to provide a lightweight JavaScript library to facilitate the process of creating SPA and/or custom elements. VJS utilize native custom elements and browser APIs which are not fully supported in all browser yet. Once those standards made it to all commonly used browsers the time to shine for VJS has come ;)
+
+### Planned Features for the first Iteration
+- ACL - Access Control Lists
+- Auth - Authentication
+- Console - debug panel for vjs components/tools
+- Form - generate, populate, validation
+- Router - manage routing for applications
+- Store - state management for application and components
+- Session - helper for session handling
+
+### v0.2-alpha
+
+- [Router](https://github.com/AndTheGodsMadeLove/vjs/tree/master/src/router)
+- [Store](https://github.com/AndTheGodsMadeLove/vjs/tree/master/src/store)
--- src/store/store.js
+++ src/store/store.js
@@ -1,3 +1,4 @@
+<<<<<<< Updated upstream
 /**
  * @version 0.1-alpha
  *
@@ -145,3 +146,178 @@ export class Store {
     return false;
   }
 }
+=======
+/**
+ * @version 0.1-alpha
+ *
+ * @requires './../util/util.js:clone
+ * @requires './../util/util.js:isEqual
+ * @requires './../util/util.js:isObject
+ */
+
+// eslint-disable-next-line
+/// <reference path='./typedef.js' />
+
+import {
+  clone,
+  isEqual,
+  isObject,
+} from './../util/util.js';
+
+/**
+ * create a new store
+ */
+export class Store {
+  /**
+   * create a new store
+   * @param {function} reducer
+   * @param {object} [initialState]
+   */
+  constructor(reducer, initialState = null) {
+    /**
+     * @property
+     * @type {function}
+     */
+    this.reducer = reducer;
+
+    /**
+     * @property
+     * @type {(object|null)}
+     */
+    this.state = null;
+
+    /**
+     * @property
+     * @type {(object|null)}
+     */
+    this.tempState = null;
+
+    /**
+     * @property
+     * @type {Array.<{ fn: function, configFn: function}>}
+     */
+    this.listenerList = [];
+
+    if (isObject(initialState)) {
+      this.setInitialState(initialState);
+    }
+  }
+
+  /**
+   * get current state
+   * @returns {object}
+   */
+  getState() {
+    if (!isObject(this.state)) {
+      throw new Error('current state is not an object! check if initial state is set and/or your reducer');
+    }
+    return clone(this.state);
+  }
+
+  /**
+   * @param {object} state
+   */
+  setInitialState(state) {
+    if (this._containsUndefined(state)) {
+      this.tempState = state;
+    } else {
+      this.state = state;
+    }
+
+    this.dispatch({}, true);
+  }
+
+  /**
+   * @returns {boolean}
+   */
+  isStateReady() {
+    return !this._containsUndefined(this.state) && isObject(this.state);
+  }
+
+  /**
+   * subscribe a callback
+   * @param {function} fn
+   * @param {function} [configFn]
+   */
+  subscribe(fn, configFn = (s) => (s)) {
+    this.listenerList.push({ fn, configFn });
+  }
+
+  /**
+   * unsubscribe a callback
+   * @param {function} fn
+   */
+  unsubscribe(fn) {
+    this.listenerList = this.listenerList.filter((listener) => listener.fn !== fn);
+  }
+
+  /**
+   * notify subscriber listening to the state or properties of the state
+   * @param {object} action - action object with type and payload
+   * @param {boolean} force - force invocation of subscribed callbacks
+   */
+  dispatch(action, force = false) {
+    let prevState = this.tempState || this.getState();
+    const nextState = this.reducer(prevState, action);
+
+    // abort if there is no state change after
+    // and the parameter force is false
+    if (!force && isEqual(prevState, nextState)) {
+      return;
+    }
+
+    // if nextState contains undefined update
+    // tempState and abort
+    if (this._containsUndefined(nextState)) {
+      this.tempState = {
+        ...this.tempState,
+        ...nextState,
+      };
+      return;
+    }
+
+    // if there is a tempState set all data are gathered
+    // now and the state is ready.
+    // * set tempState back to null (use state object instead of tempState object)
+    // * fill state object with gathered data
+    // * simulate empty prevState
+    if (this.tempState !== null) {
+      this.tempState = null;
+      this.state = nextState;
+      prevState = {};
+    }
+
+    // iterate listener
+    this.listenerList.forEach((listener) => {
+      const prevValues = listener.configFn(prevState);
+      const nextValues = listener.configFn(nextState);
+      // abort if observed property has no state change
+      if (!force && isEqual(prevValues, nextValues)) {
+        return;
+      }
+
+      // invoke callback
+      listener.fn(nextState);
+    });
+  }
+
+  /**
+   * check if a object contains a undefined property
+   * @param {object} o
+   * @returns {boolean}
+   */
+  _containsUndefined(o) {
+    for (const i in o) {
+      if (typeof o[i] === 'undefined') {
+        return true;
+      }
+
+      if (isObject(o[i])) {
+        return this._containsUndefined(o[i]);
+      }
+    }
+
+    return false;
+  }
+}
+>>>>>>> Stashed changes
